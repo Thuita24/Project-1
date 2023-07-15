63d17eaa0c10d9869b68d2c3b822e1116c27441b
Thuita24 <131273659+Thuita24@users.noreply.github.com>
2023-07-15 15:27:47 +0300

    Add files via upload

---
+++ Pharmacist.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Pharmacist form</title>
+    </head>
+
+    <body>
+        <h1>Pharmacist form</h1>
+        <form action="addPharmacist.php" method="POST" autocomplete="on">
+<div>
+    <label for="SSN" >SSN:</label>
+    <input type="number" id="SSN" name="SSN">
+</div>
+
+<div>
+    <label for="pharmacist_name">pharmacist_name:</label>
+    <input type="text" id="pharmacist_name" name="pharmacist_name">
+</div>
+
+<div>
+<input type="Submit" name="btnsubmit" />
+</div>
+
+
+    </body>
+</form>
+    </html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</html>
